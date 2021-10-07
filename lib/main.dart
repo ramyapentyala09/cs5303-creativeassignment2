1da0185cf6758241671d72cd34cea9bf937e63dc
Ramya Pentyala <ramyapentyala@uco.edu>
2021-10-06 19:54:03 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:assignmenttwo/pages/bmi/input_page.dart';
 import 'package:assignmenttwo/pages/home/home.dart';
 import 'package:assignmenttwo/pages/profile/profile.dart';
 import 'package:assignmenttwo/pages/signin/signin.dart';
-import 'package:assignmenttwo/pages/splash/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -16,9 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BMI calculator',
-      initialRoute: SplashScreen.routeName,
+      initialRoute: SignInScreen.routeName,
       routes: {
-        SplashScreen.routeName: (context) => SplashScreen(),
         SignInScreen.routeName: (context) => SignInScreen(),
         HomeScreen.routeName: (context) => HomeScreen(),
         InputPage.routeName: (context) => InputPage(),
